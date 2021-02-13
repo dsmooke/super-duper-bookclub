598d0b0075675eec9d55750e4990b97a666eaec0
Danny Smooke <dana.smooke@gmail.com>
2021-02-13 11:30:01 -0500

    fixed duplicate not, changed to NULL - good

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE bookshelf_db;
 CREATE TABLE bookshelf (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
-    author VARCHAR(30) NOT NOT,
+    author VARCHAR(30) NOT NULL,
     genre VARCHAR(30),
     PRIMARY KEY (id)
 );
