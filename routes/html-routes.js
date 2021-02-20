200a8281cabb6d1148beb264848bebc791d2c70c
Danny Smooke <dana.smooke@gmail.com>
2021-02-20 14:14:59 -0500

    removed code to be filled with next merge request

---
+++ routes/html-routes.js
@@ -0,0 +1,9 @@
+// Dependencies
+const path = require("path");
+
+// Routes
+
+//save-book.js will save books to wishlist
+
+// routes look inside views (sees layout and partials folder)
+// views look inside partials
